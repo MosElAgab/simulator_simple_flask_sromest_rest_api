002c2a3947faa07ea7a899e751753c7837d97a95
MosElAgab <mostafa.elagab@gmail.com>
2025-07-20 16:17:20 +0100

    adds type hints for store_simulator

--- store_simulator.py
+++ store_simulator.py
@@ -6,22 +6,22 @@ faker = Faker()
 
 class StoreSimulator:
 
-    def __init__(self, base_url, authorization_header):
+    def __init__(self, base_url: str, authorization_header: dict):
         self.base_url = base_url
         self.authorization_header = authorization_header
 
 
-    def get_stores(self):
+    def get_stores(self) -> list:
         response = requests.get(f"{self.base_url}/store")
         return response.json()
 
 
-    def get_store_ids(self, stores):
+    def get_store_ids(self, stores: list) -> list:
         store_ids = [store["store_id"] for store in stores]
         return store_ids
 
 
-    def create_store_data(self):
+    def create_store_data(self) -> dict:
         store_name = faker.company() + "-" + uuid.uuid4().hex[0:2]
         data = {
             "store_name": store_name
@@ -29,7 +29,7 @@ class StoreSimulator:
         return data
 
 
-    def create_store(self, store_data):
+    def create_store(self, store_data: dict) -> dict:
         response = requests.post(f"{self.base_url}/store", json=store_data, headers=self.authorization_header)
         if response.ok:
             print("Create store:", response.status_code, response.json())
@@ -39,7 +39,7 @@ class StoreSimulator:
         return response.json()
 
 
-    def create_many_stores(self, number_of_store):
+    def create_many_stores(self, number_of_store: int) -> None:
         for i in range(number_of_store):
             store_data = self.create_store_data()
             self.create_store(store_data)
